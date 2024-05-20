e62e29cac5e062af52b8aa45d43ee01a56aa9204
GoncaloCVicente05 <05goncalovicente@gmail.com>
2024-05-20 11:27:30 +0100

    Controller-h
    file completed

--- Headers/Controllers/Controller.h
+++ Headers/Controllers/Controller.h
@@ -5,4 +5,31 @@
 #ifndef WALLET_MANAGEMENT_CONTROLLER_H
 #define WALLET_MANAGEMENT_CONTROLLER_H
 
+#include "View.h"
+#include "AcquireView.h"
+#include "AccountView.h"
+#include "ClientView.h"
+#include "InsuranceView.h"
+#include "LoanView.h"
+#include "TransactionsView.h"
+#include "WalletManagement.h"
+
+class Controller{
+
+private:
+    View view;
+    AcquireView acquireView;
+    AccountView accountView;
+    ClientView clientView;
+    InsuranceView insuranceView;
+    LoanView loanView;
+    TransactionsView transactionsView;
+    WalletManagement model;
+    void runAccount(), runClient(), runLoan(), runTransactions(), runInsurance();
+
+public:
+    Controller(WalletManagement& walletManagement);
+    void run();
+};
+
 #endif //WALLET_MANAGEMENT_CONTROLLER_H
--- Sources/Controllers/Controller.cpp
+++ Sources/Controllers/Controller.cpp
@@ -1,3 +1,11 @@
 //
 // Created by corre on 20/05/2024.
 //
+#include <iostream>
+#include <string>
+#include <tuple>
+#include "Controller.h"
+#include "Utils.h"
+#include "DataConsistencyException.h"
+
+
