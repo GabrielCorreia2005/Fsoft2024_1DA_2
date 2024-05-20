190b1d3f49451de132e767db35b7b6a9c833baec
GoncaloCVicente05 <05goncalovicente@gmail.com>
2024-05-20 11:42:39 +0100

    Update accounts.h
    accounts update but not finalized

--- Headers/Model/Accounts.h
+++ Headers/Model/Accounts.h
@@ -5,4 +5,25 @@
 #ifndef WALLET_MANAGEMENT_ACCOUNTS_H
 #define WALLET_MANAGEMENT_ACCOUNTS_H
 
+#include "Client.h"
+
+using namespace std;
+
+class Accounts{
+
+private:
+    int nr;
+    float balance;
+    Client * client;
+    bool isPointerNotNull(void * pointer);
+
+public:
+    Accounts(int nr, float balance, Client * client);
+    Accounts(const Accounts& obj);
+    ~Accounts();
+
+    
+};
+
+
 #endif //WALLET_MANAGEMENT_ACCOUNTS_H
