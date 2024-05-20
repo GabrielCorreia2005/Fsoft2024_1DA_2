fbff9f7016ccc9a9752f9677c9429cfbd3776649
Gabriel Correia <correiaalves.gabriel@gmail.com>
2024-05-20 11:28:59 +0100

    exceptions

--- Sources/Exceptions/DuplicateDataException.cpp
+++ Sources/Exceptions/DuplicateDataException.cpp
@@ -1,3 +1,9 @@
 //
 // Created by corre on 20/05/2024.
 //
+
+#include "DuplicatedDataException.h"
+
+DuplicatedDataException::DuplicatedDataException(const string &what_arg) : runtime_error(what_arg) {}
+
+DuplicatedDataException::DuplicatedDataException(const char *what_arg) : runtime_error(what_arg) {}
--- Sources/Exceptions/InvalidDataExcption.cpp
+++ Sources/Exceptions/InvalidDataExcption.cpp
@@ -1,3 +1,8 @@
 //
 // Created by corre on 20/05/2024.
 //
+#include "InvalidDataException.h"
+
+InvalidDataException::InvalidDataException(const string &what_arg) : runtime_error(what_arg) {}
+
+InvalidDataException::InvalidDataException(const char *what_arg) : runtime_error(what_arg) {}
