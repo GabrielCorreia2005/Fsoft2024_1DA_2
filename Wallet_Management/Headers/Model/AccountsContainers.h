5cd6b982ad41eeb9f7a05810a69dbf12d2e9c381
GoncaloAzevedo05 <160490124+GoncaloAzevedo05@users.noreply.github.com>
2024-05-27 11:35:18 +0100

    (Accounts, Accountscontainer e client).h e accounts.cpp

--- Wallet_Management/Headers/Model/AccountsContainers.h
+++ Wallet_Management/Headers/Model/AccountsContainers.h
@@ -5,4 +5,23 @@
 #ifndef WALLET_MANAGEMENT_ACCOUNTSCONTAINERS_H
 #define WALLET_MANAGEMENT_ACCOUNTSCONTAINERS_H
 
+#include <list>
+#include "Accounts.h"
+
+class AccountsContainer{
+private:
+    list<Accounts> accounts;
+    list<Accounts>::iterator search(int &nr);
+
+public:
+    list<Accounts> getAll();
+    Instructor* get(int &nr);
+    void add(const Accounts& obj);
+    void remove(int &nr);
+    void update(int nr, float balance, Client * client);
+    bool  isThereSubject(int &nr);
+
+};
+
+
 #endif //WALLET_MANAGEMENT_ACCOUNTSCONTAINERS_H
--- Wallet_Management/Headers/Model/Client.h
+++ Wallet_Management/Headers/Model/Client.h
@@ -2,7 +2,28 @@
 // Created by corre on 20/05/2024.
 //
 
-#ifndef WALLET_MANAGEMENT_CLIENT_H
-#define WALLET_MANAGEMENT_CLIENT_H
+#ifndef WALLET_MANAGEMENT_ACCOUNTS_H
+#define WALLET_MANAGEMENT_ACCOUNTS_H
 
-#endif //WALLET_MANAGEMENT_CLIENT_H
+#include "Client.h"
+
+using namespace std;
+
+class Client{
+
+private:
+    char name
+    int number;
+    float birth;
+    bool isPointerNotNull(void * pointer);
+
+public:
+    Client(char name, int nr, float birth);
+    Accounts(const Client& obj);
+    ~Client();
+
+
+};
+
+
+#endif //WALLET_MANAGEMENT_ACCOUNTS_H
