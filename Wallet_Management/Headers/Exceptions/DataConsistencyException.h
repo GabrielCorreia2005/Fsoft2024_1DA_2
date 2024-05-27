43dcaf1bdc0e2354b8dea31205cdac0afdb77707
Gabriel Correia <correiaalves.gabriel@gmail.com>
2024-05-27 11:11:12 +0100

    sbdfhds

--- Wallet_Management/Headers/Exceptions/DataConsistencyException.h
+++ Wallet_Management/Headers/Exceptions/DataConsistencyException.h
@@ -5,4 +5,18 @@
 #ifndef WALLET_MANAGEMENT_DATACONSISTENCYEXCEPTION_H
 #define WALLET_MANAGEMENT_DATACONSISTENCYEXCEPTION_H
 
+#include <exception>
+#include <string>
+using namespace std;
+
+class DataConsistencyException : public exception {
+private:
+    string data;
+public:
+    explicit DataConsistencyException(const string &data);
+
+    const char *what()  override;
+};
+
+
 #endif //WALLET_MANAGEMENT_DATACONSISTENCYEXCEPTION_H
--- Wallet_Management/Sources/Exceptions/DataConsistencyExceprion.cpp
+++ Wallet_Management/Sources/Exceptions/DataConsistencyExceprion.cpp
@@ -1,15 +1,14 @@
-//
-// Created by Gabriel on 20/05/2024.
-//
+/*
+ * DataConsistencyException.cpp
+ *
+ *  Created on: 13/05/2024
+ *      Author: gvice
+ */
+
 #include "DataConsistencyException.h"
 
-DataConsistencyException::DataConsistencyException(string data) : data(data) {}
+DataConsistencyException::DataConsistencyException(const string &data) : data(data) {}
 
-const char* DataConsistencyException::what() const noexcept {
-return ("Data consistency error: " + data).c_str();
+const char *DataConsistencyException::what() {
+    return data.c_str();
 }
-
-//how to use
-//if (student.getAge() < subject.getMinimumAge()) {
-//    throw DataConsistencyException("Student's age is below the minimum age for this subject.");
-//}
