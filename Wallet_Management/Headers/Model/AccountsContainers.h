02619f93113d331cb26817f21c9ca99725f3835c
Gabriel Correia <correiaalves.gabriel@gmail.com>
2024-05-27 11:57:11 +0100

    model update

--- Wallet_Management/Headers/Model/AccountsContainers.h
+++ Wallet_Management/Headers/Model/AccountsContainers.h
@@ -5,4 +5,31 @@
 #ifndef WALLET_MANAGEMENT_ACCOUNTSCONTAINERS_H
 #define WALLET_MANAGEMENT_ACCOUNTSCONTAINERS_H
 
+#include <list>
+#include "Accounts.h"
+
+using namespace std;
+
+class AccountsContainers {
+private:
+    list<Accounts> accounts;
+
+public:
+    // Get a list of all accounts
+    list<Accounts> getAll();
+
+    // Get a specific account by account number
+    Accounts* get(int nr);
+
+    // Add a new account to the container
+    void add(const Accounts &obj);
+
+    // Remove an account by account number
+    void remove(int nr);
+
+    // Update the balance of an existing account
+    void update(int nr, float balance);
+};
+
+
 #endif //WALLET_MANAGEMENT_ACCOUNTSCONTAINERS_H
--- Wallet_Management/Sources/Model/AccountsContainers.cpp
+++ Wallet_Management/Sources/Model/AccountsContainers.cpp
@@ -1,3 +1,55 @@
 //
-// Created by corre on 20/05/2024.
+// Created by gvice on 13/05/2024.
 //
+
+#include "AccountsContainers.h"
+#include "DuplicatedDataException.h"
+
+list<Accounts> AccountsContainers::getAll() {
+    return accounts;
+}
+
+Accounts* AccountsContainers::get(int nr) {
+    for (auto it = accounts.begin(); it != accounts.end(); ++it) {
+        if (it->getNr() == nr) {
+            return &(*it); // Return a pointer to the found Account object
+        }
+    }
+    return nullptr; // Return nullptr if no account with the given number is found
+}
+
+void AccountsContainers::add(const Accounts &obj) {
+    if (get(obj.getNr()) != nullptr) { // Check if an account with the same number already exists
+        throw DuplicatedDataException("Account with this number already exists.");
+    }
+    accounts.push_back(obj);
+}
+
+void AccountsContainers::remove(int nr) {
+    for (auto it = accounts.begin(); it != accounts.end(); ++it) {
+        if (it->getNr() == nr) {
+            accounts.erase(it);
+            return; // Account removed, exit the function
+        }
+    }
+    // No account found with the given number
+}
+
+void AccountsContainers::update(int nr, float balance) {
+    for (auto it = accounts.begin(); it != accounts.end(); ++it) {
+        if (it->getNr() == nr) {
+            // Assuming you have a setBalance method in the Accounts class
+            it->setBalance(balance);
+            return; // Account updated, exit the function
+        }
+    }
+    // No account found with the given number
+}
+
+// Example of a setBalance method within the Accounts class:
+void Accounts::setBalance(float newBalance) {
+    if (newBalance < 0) {
+        throw DataConsistencyException("Balance cannot be negative.");
+    }
+    balance = newBalance;
+}
