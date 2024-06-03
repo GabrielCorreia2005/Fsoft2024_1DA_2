56b8e2cb2442ddc88dd8e7aa2a5db3667e21e499
GoncaloCVicente05 <05goncalovicente@gmail.com>
2024-06-03 10:29:25 +0100

    update main.cpp

--- Wallet_Management/main.cpp
+++ Wallet_Management/main.cpp
@@ -1,3 +1,20 @@
 //
 // Created by gvice on 13/05/2024.
 //
+
+#include <iostream>
+#include "Wallet_Management.h"
+
+#include "Controller.h"
+using namespace std;
+
+int main()
+{
+    Wallet_Management walletManagement ("Wallet Management");
+
+    Controller controller (walletManagement);
+    controller.run();
+
+    cout << "Aplication has existed" << endl;
+    return 0;
+}
