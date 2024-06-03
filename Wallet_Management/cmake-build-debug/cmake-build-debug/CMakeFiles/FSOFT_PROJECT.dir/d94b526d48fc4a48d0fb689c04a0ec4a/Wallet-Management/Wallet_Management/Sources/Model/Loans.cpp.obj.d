369b9f63b6dcd21282dd76245b5048099fd2ba02
GoncaloAzevedo05 <160490124+GoncaloAzevedo05@users.noreply.github.com>
2024-06-03 19:12:41 +0100

    Erro2

--- Wallet_Management/cmake-build-debug/cmake-build-debug/CMakeFiles/FSOFT_PROJECT.dir/d94b526d48fc4a48d0fb689c04a0ec4a/Wallet-Management/Wallet_Management/Sources/Model/Loans.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FSOFT_PROJECT.dir/d94b526d48fc4a48d0fb689c04a0ec4a/Wallet-Management/Wallet_Management/Sources/Model/Loans.cpp.obj: \
- D:\ISEP\Fundamenos\ de\ software\Wallet\ Management2\Wallet-Management\Wallet_Management\Sources\Model\Loans.cpp
