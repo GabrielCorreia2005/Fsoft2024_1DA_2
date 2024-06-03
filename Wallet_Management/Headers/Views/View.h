c2dea0b033542686df06dc637517d8c8c88e2c28
JosePerfeito <1231081@isep.ipp.pt>
2024-06-03 11:36:16 +0100

    View.h
    Udate View.h

--- Wallet_Management/Headers/Views/View.h
+++ Wallet_Management/Headers/Views/View.h
@@ -5,4 +5,12 @@
 #ifndef FSOFT_PROJECT_VIEW_H
 #define FSOFT_PROJECT_VIEW_H
 
+using namespace std;
+class View{
+public:
+    View();
+    int MenuClient();
+    int MenuBank();
+
+
 #endif //FSOFT_PROJECT_VIEW_H
