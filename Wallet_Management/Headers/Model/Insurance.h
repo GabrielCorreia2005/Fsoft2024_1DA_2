5e68d6bbf91ca9641631eb8d1576ce0d4992acc8
JosePerfeito <1231081@isep.ipp.pt>
2024-06-03 16:25:41 +0100

    headers
    Update insurance.h loans.h Transctions.h

--- Wallet_Management/Headers/Model/Insurance.h
+++ Wallet_Management/Headers/Model/Insurance.h
@@ -5,4 +5,15 @@
 #ifndef WALLET_MANAGEMENT_INSURANCE_H
 #define WALLET_MANAGEMENT_INSURANCE_H
 
+using namespace std;
+
+class Insurance{
+public:
+    float value;
+    float lentgh;
+private:
+    Insurance(float value, float lentgh);
+    Insurance(const Insurance &obj);
+    ~Insurance();
+};
 #endif //WALLET_MANAGEMENT_INSURANCE_H
--- Wallet_Management/Headers/Model/Transactions.h
+++ Wallet_Management/Headers/Model/Transactions.h
@@ -5,4 +5,20 @@
 #ifndef WALLET_MANAGEMENT_TRANSACTIONS_H
 #define WALLET_MANAGEMENT_TRANSACTIONS_H
 
+#include <string>
+#include "Date.h"
+using namespace std;
+
+class Transactions{
+private :
+    long int id;
+    float amount;
+    Date date;
+    string type;
+public:
+    Transactions( long int id,  float amount, const Date& date,string type);
+    Transactions(const Transactions &obj);
+    ~Transactions();
+};
+
 #endif //WALLET_MANAGEMENT_TRANSACTIONS_H
