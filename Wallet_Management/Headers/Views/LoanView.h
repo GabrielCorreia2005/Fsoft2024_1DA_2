6a11dea79de901d0c9e8c5bd50417d2adc1b53c4
Gabriel Correia <correiaalves.gabriel@gmail.com>
2024-06-04 10:57:20 +0100

    account and loans //view

--- Wallet_Management/Headers/Views/LoanView.h
+++ Wallet_Management/Headers/Views/LoanView.h
@@ -5,4 +5,23 @@
 #ifndef FSOFT_PROJECT_LOANVIEW_H
 #define FSOFT_PROJECT_LOANVIEW_H
 
+#include <list>
+#include "Loans.h"
+
+class LoanView {
+public:
+    // Get loan data from user input
+    Loans getLoan();
+
+    // Print details of a single loan to the console
+    void printLoan(Loans *loan);
+
+    // Print a list of loans to the console
+    void printLoans(list<Loans> &loans);
+
+    // You might want to add functions like:
+    // void printClientLoans(Client * client, list<Loans>& loans); // To print loans for a specific client
+    // Loans* selectLoan(list<Loans>& loans); // To allow user to select a loan from a list
+};
+
 #endif //FSOFT_PROJECT_LOANVIEW_H
