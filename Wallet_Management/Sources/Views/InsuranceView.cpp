602499115e021d051c89279283ce0aad3d34c0a4
GoncaloCVicente05 <05goncalovicente@gmail.com>
2024-06-04 10:30:27 +0100

    log files

--- Wallet_Management/Sources/Views/InsuranceView.cpp
+++ Wallet_Management/Sources/Views/InsuranceView.cpp
@@ -1,3 +1,14 @@
 //
 // Created by gvice on 20/05/2024.
 //
+#include <iostream>
+#include "InsuranceView.h"
+#include "Utils.h"
+#include "InvalidDataException.h"
+
+using namespace std;
+
+Insurance InsuranceView::getInsurance()
+{
+    
+}
