db22c5c2dc145b1c59a6fe1b3de296317e4fe9f3
JosePerfeito <1231081@isep.ipp.pt>
2024-06-06 11:22:34 +0100

    Insurance View
    InsuranceView

--- Wallet_Management/Sources/Views/InsuranceView.cpp
+++ Wallet_Management/Sources/Views/InsuranceView.cpp
@@ -1,33 +1,56 @@
-//
-// Created by gvice on 20/05/2024.
-//
-#include <iostream>
 #include "InsuranceView.h"
 #include "Utils.h"
-#include "InvalidDataException.h"
-#include "Insurance.h";
+#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-Insurance insuranceView::getInsurance();
-{
-    Insurance insurance("Temporary name", price, length, installments, fees);
-    bool flag = false;
+Insurance InsuranceView::getInsurance() {
+    string name;
+    float price;
+    float length;
 
-    do
-    {
-        try
-        {
-            flag = false;
+    // Get insurance name
+    name = Utils::getString("Enter insurance name: ");
 
-            cout << "Insurance" << endl;
-
-            string Ins_name = Utils::getString("Insurance Name");
-
-
-        }catch(InvalidDataException& e)
-        {
-            flag = true;
+    // Get insurance price
+    do {
+        price = Utils::getNumber("Enter insurance price: ");
+        if (price <= 0) {
+            cout << "Invalid insurance price. Please enter a positive number." << endl;
         }
-    }while(flag == true);
+    } while (price <= 0);
+
+    // Get insurance length
+    do {
+        length = Utils::getNumber("Enter insurance length (in months): ");
+        if (length <= 0) {
+            cout << "Invalid insurance length. Please enter a positive number." << endl;
+        }
+    } while (length <= 0);
+
+    return Insurance(name, price, length);
+}
+
+void InsuranceView::printInsurance(Insurance *insurance) {
+    if (insurance) {
+        cout << "Insurance Name: " << insurance->getName() << endl;
+        cout << "Price: " << fixed << setprecision(2) << insurance->getPrice() << endl;
+        cout << "Length (months): " << fixed << setprecision(2) << insurance->getLength() << endl;
+    } else {
+        cout << "Invalid Insurance (nullptr)." << endl;
+    }
+}
+
+void InsuranceView::printInsurances(list<Insurance> &insurances) {
+    if (insurances.empty()) {
+        cout << "There are no insurances to display." << endl;
+        return;
+    }
+
+    cout << "List of Insurances:" << endl;
+    for (const Insurance &insurance : insurances) {
+        printInsurance(&insurance);
+        cout << "--------------------" << endl;
+    }
 }
