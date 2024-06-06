87b714e29ae73d50ca8ea3137dde57bbbb52cad1
Gabriel Correia <correiaalves.gabriel@gmail.com>
2024-06-06 11:06:26 +0100

    mock.h

--- Wallet_Management/Mock/MockData.h
+++ Wallet_Management/Mock/MockData.h
@@ -5,4 +5,12 @@
 #ifndef FSOFT_PROJECT_MOCKDATA_H
 #define FSOFT_PROJECT_MOCKDATA_H
 
+#include "WalletManagement.h"
+
+class MockData {
+public:
+    // Generates mock data for the WalletManagement system
+    void generateData(WalletManagement &walletManagement);
+};
+
 #endif //FSOFT_PROJECT_MOCKDATA_H
