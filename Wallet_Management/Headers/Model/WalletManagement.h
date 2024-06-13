6d3b94cfab7d0cbb050e1f35da9ba80cbb7d8341
Gabriel Correia <correiaalves.gabriel@gmail.com>
2024-06-13 15:51:42 +0100

    wallet.h

---
+++ Wallet_Management/Headers/Model/WalletManagement.h
@@ -0,0 +1,39 @@
+//
+// Created by Gabriel on 20/05/2024.
+//
+
+#ifndef WALLET_MANAGEMENT_WALLETMANAGEMENT_H
+#define WALLET_MANAGEMENT_WALLETMANAGEMENT_H
+
+#include "ClientsContainer.h"
+#include "AccountsContainers.h"
+#include "LoansContainers.h"
+#include "InsuranceContainer.h"
+#include "TransactionsContainers.h"
+
+class WalletManagement {
+private:
+    ClientsContainer clientsContainer;
+    AccountsContainers accountsContainer;
+    LoansContainers loansContainer;
+    InsuranceContainer insuranceContainer;
+    TransactionsContainers transactionsContainer;
+
+    void setDataForConsistency(); // Function to check and enforce data consistency
+
+public:
+    // Constructors
+    WalletManagement();
+    WalletManagement(const char *obj);
+
+    // Getters for the containers
+    ClientsContainer &getClientContainer();
+    AccountsContainers &getAccountsContainer();
+    LoansContainers &getLoansContainer();
+    InsuranceContainer &getInsuranceContainer();
+    TransactionsContainers &getTransactionsContainer();
+
+    //por adicionar mais
+};
+
+#endif //WALLET_MANAGEMENT_WALLETMANAGEMENT_H
---
+++ Wallet_Management/cmake-build-debug/cmake-build-debug/CMakeFiles/FSOFT_PROJECT.dir/4f29e1046087d4cedaf8c1f9b09a740e/Wallet_Management_Saver/Sources/Model/WalletManagement.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/FSOFT_PROJECT.dir/4f29e1046087d4cedaf8c1f9b09a740e/Wallet_Management_Saver/Sources/Model/WalletManagement.cpp.obj: \
+ D:\ISEP\Fundamenos\ de\ software\Wallet\ Management2\Wallet-Management\Wallet_Management_Saver\Sources\Model\WalletManagement.cpp \
+ D:/ISEP/Fundamenos\ de\ software/Wallet\ Management2/Wallet-Management/Wallet_Management/Headers/Model/WalletManagement.h
