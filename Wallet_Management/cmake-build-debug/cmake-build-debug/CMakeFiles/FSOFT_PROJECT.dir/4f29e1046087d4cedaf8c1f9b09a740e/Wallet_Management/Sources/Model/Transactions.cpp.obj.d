5c998557e8f0aced47209c209520452afccd9174
GoncaloCVicente05 <05goncalovicente@gmail.com>
2024-06-15 14:49:24 +0100

    updates

--- Wallet_Management/cmake-build-debug/cmake-build-debug/CMakeFiles/FSOFT_PROJECT.dir/4f29e1046087d4cedaf8c1f9b09a740e/Wallet_Management/Sources/Model/Transactions.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FSOFT_PROJECT.dir/4f29e1046087d4cedaf8c1f9b09a740e/Wallet_Management/Sources/Model/Transactions.cpp.obj: \
- D:\ISEP\Fundamenos\ de\ software\Wallet\ Management2\Wallet-Management\Wallet_Management\Sources\Model\Transactions.cpp
