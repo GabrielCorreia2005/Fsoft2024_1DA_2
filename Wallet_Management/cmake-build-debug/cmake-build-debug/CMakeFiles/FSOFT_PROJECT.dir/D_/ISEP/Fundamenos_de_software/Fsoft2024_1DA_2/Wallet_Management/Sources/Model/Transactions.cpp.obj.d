2d15614c40be5f6831a37b462f75f256ac02c93a
GoncaloCVicente05 <05goncalovicente@gmail.com>
2024-06-15 16:32:36 +0100

    updates

--- Wallet_Management/cmake-build-debug/cmake-build-debug/CMakeFiles/FSOFT_PROJECT.dir/D_/ISEP/Fundamenos_de_software/Fsoft2024_1DA_2/Wallet_Management/Sources/Model/Transactions.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FSOFT_PROJECT.dir/D_/ISEP/Fundamenos_de_software/Fsoft2024_1DA_2/Wallet_Management/Sources/Model/Transactions.cpp.obj: \
- D:\ISEP\Fundamenos\ de\ software\Fsoft2024_1DA_2\Wallet_Management\Sources\Model\Transactions.cpp
--- Wallet_Management/cmake-build-debug/cmake-build-debug/CMakeFiles/FSOFT_PROJECT.dir/D_/ISEP/Fundamenos_de_software/Fsoft2024_1DA_2/Wallet_Management/Sources/Model/TransactionsContainers.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FSOFT_PROJECT.dir/D_/ISEP/Fundamenos_de_software/Fsoft2024_1DA_2/Wallet_Management/Sources/Model/TransactionsContainers.cpp.obj: \
- D:\ISEP\Fundamenos\ de\ software\Fsoft2024_1DA_2\Wallet_Management\Sources\Model\TransactionsContainers.cpp
